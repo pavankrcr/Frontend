1b4d0eed32c07a02269dbae158155c07aeb95c3e
Pavank <mail@pavank.in>
2023-05-15 21:50:07 +0530

    first commit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className=heading> GST Billing DashBoard</h1>;
+    </>
+  )
+}
+
+export default App
